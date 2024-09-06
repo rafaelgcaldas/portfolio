f181e7afe9d05e24fd2b1ef38d91b3adbc97a784
Rafael Gomes <rafael.gcaldas01@gmail.com>
2024-09-05 23:52:57 -0300

    fix: fix suppressHydrationWarning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,10 @@
+import { BackToTop } from '@/components/back-to-top'
 import { Footer } from '@/components/footer'
 import { Header } from '@/components/header'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { Providers } from './providers'
-import { BackToTop } from '@/components/back-to-top'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="pt" className="dark scroll-smooth">
+    <html lang="pt" suppressHydrationWarning className="scroll-smooth">
       <body className={`${inter.className} antialiased`}>
         <Providers>
           <Header />
